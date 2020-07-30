02734f07ca1ac7bfa7630e453129e69febe4fc93
Vahid <vahid.vaezian@gmail.com>
2020-07-29 21:31:45 -0700

    Update TimeSeries.py

--- TimeSeries.py
+++ TimeSeries.py
@@ -7,4 +7,5 @@ time_stamp.year # .month .week .day
 time_stamp.day_name()  # Friday
 time_stamp.weekday()  # 4  (starts from Monday=0)
 
-period = pd.Period('2020-06', 'M')  # M (month) is the default, so we can skip it
+period = pd.Period('2020-06', 'M')  # If we omit 'M', it defaults to it. The defaul frequency for '2020-06-01' is day
+period.asfreq('D')  # Period('2020-06-30', 'D')
