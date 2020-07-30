e5ed287a05c595c2712d273373ac42faa6da5cfb
Vahid <vahid.vaezian@gmail.com>
2020-07-29 22:47:06 -0700

    Update TimeSeries.py

--- TimeSeries.py
+++ TimeSeries.py
@@ -21,3 +21,10 @@ index = pd.date_range('2020-06-12', periods=3, freq='D')  # DatetimeIndex(['2020
 index[0]  # Timestamp('2020-06-12 00:00:00', freq='D')
 period_index = index.to_period()  # PeriodIndex(['2020-06-12', '2020-06-13', '2020-06-14'], dtype='period[D]', freq='D')
 period[0]  # Period('2020-06-12', 'D')
+
+data = np.random.random((3,2))  # array of random numbers, 3 rows, 2 columns
+pd.DataFrame(data, index=index)
+ 	              0 	      1
+2020-06-12 	0.160132 	0.500385
+2020-06-13 	0.287287 	0.539113
+2020-06-14 	0.749582 	0.899299
