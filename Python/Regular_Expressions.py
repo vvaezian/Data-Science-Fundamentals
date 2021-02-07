3a5672f1487fc071c949e396a3396a04961a16e2
Vahid <vahid.vaezian@gmail.com>
2021-02-06 21:19:00 -0800

    Update Regular_Expressions.py

--- Python/Regular_Expressions.py
+++ Python/Regular_Expressions.py
@@ -1,11 +1,18 @@
+# Cheetsheet: https://www.debuggex.com/cheatsheet/regex/python
+
 # cappital letters negate. For example \W is negation of \w
 # \w = [A-Za-z0-9_]
+# . = Any character except newline
 
 import re
 msg = "hi, this is a test"
+re.findall('\w*', msg)  # ['hi', '', '', 'this', '', 'is', '', 'a', '', 'test', '']
+re.findall('\w+', msg)  # ['hi', 'this', 'is', 'a', 'test'] 
+re.findall('.*', msg)  # ['hi, this is a test', '']
+re.findall('.+', msg)  # ['hi, this is a test']
 re.findall('.*s', msg)  # ['hi, this is a tes']
 re.findall('.*?s', msg)  # ['hi, this', ' is', ' a tes']  non-greedy search
-re.findall('\w+', msg)  # ['hi', 'this', 'is', 'a', 'test'] 
+
 # Return the first match. If it doesn't match anything, returns None. So it can be used in conditionals
 re.match('.*"(.*),(.*).*"', row)  
 # refer to matched groups using \g<n> (\n also works but is ambiguous when number is in the text) 
