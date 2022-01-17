d91bb32a2c7ba12f3a98b239e4cc10a0e7a94f54
Vahid <vahid.vaezian@gmail.com>
2022-01-16 22:51:55 -0800

    Update scipy.py

--- scipy.py
+++ scipy.py
@@ -1,4 +1,6 @@
-# approximating 1/x with polynomials
+# Approximating 1/x with polynomials
+# i.e. finding the weights vector W that minimizes 
+# 1/X - W * P(X)
 
 from sklearn.metrics import mean_squared_error
 
@@ -9,7 +11,7 @@ def objective_function(W, X, Y):
 from scipy.optimize import minimize
 
 X = np.array([[1, x, x**2, x**3] for x in range(1, 100)])
-Y = 1/X[:, 1]
+Y = 1/X[:, 1]  # true values
 W_init = np.array([1] * len(X[0]))  # initial weights
 result = minimize(objective_function, W_init, args=(X,Y), options={'maxiter': 5000})
 W_hat = result.x  # learned weights
