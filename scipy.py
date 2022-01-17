a25f09641e09b859451ee43af29a563e53628a7e
Vahid <vahid.vaezian@gmail.com>
2022-01-16 22:52:34 -0800

    Update scipy.py

--- scipy.py
+++ scipy.py
@@ -3,15 +3,15 @@
 # 1/X - W * P(X)
 
 from sklearn.metrics import mean_squared_error
+from scipy.optimize import minimize
 
 def objective_function(W, X, Y):
     error = mean_squared_error(Y, np.matmul(X, W))
     return(error)
-  
-from scipy.optimize import minimize
 
 X = np.array([[1, x, x**2, x**3] for x in range(1, 100)])
 Y = 1/X[:, 1]  # true values
+
 W_init = np.array([1] * len(X[0]))  # initial weights
 result = minimize(objective_function, W_init, args=(X,Y), options={'maxiter': 5000})
 W_hat = result.x  # learned weights
